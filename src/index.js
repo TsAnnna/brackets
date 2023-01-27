8f231fb772ab869534cba3b31352a4c3142c3c21
Anna <green.abrikos.73@gmail.com>
2023-01-27 16:53:18 +0300

    feat: the script executes 12 out of 20 successfully

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split("");
+  let result = "";
+  
+  // if(str.length % 2 != 0) return false;
+  bracketsConfig.forEach((item, index, arr) => {
+    // console.log(str);
+    if(str.includes(item[0]) && str.includes(item[1])) {
+      
+      str.splice(str.indexOf(item[0]), 1);
+      str.splice(str.indexOf(item[1]), 1);
+      // console.log(str);
+      // return;
+    }
+  }
+  );
+  if(!str.length){
+    return true
+  }
+  return false;
 }
+
+
+// function check(str, bracketsConfig) {
+//   str = str.split("");
+//   let result = "";
+  
+//   if(str.length % 2 != 0) return false;
+//   bracketsConfig.forEach((item, index, arr) => {
+//     // console.log(str);
+//     if(str.includes(item[0]) && str.includes(item[1])) {
+      
+//       str.splice(str.indexOf(item[0]), 1);
+//       str.splice(str.indexOf(item[1]), 1);
+//       // console.log(str);
+//       // return;
+//     }
+//   }
+//   );
+//   if(!str.length){
+//     return true
+//   }
+//   return false;
+// }
+
+// console.log(check('[(])', [ ['(', ')'], ['[', ']'] ]));
